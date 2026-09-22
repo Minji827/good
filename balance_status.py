5c4ea1f0a6a957474070c9dc1dd7e85fecfd52a7
Minji827 <gminji119@gmail.com>
2026-09-22 14:04:34 +0900

    fix

--- balance_status.py
+++ balance_status.py
@@ -1,11 +1,8 @@
 def balance_status():
     balance = int(input("잔액을 입력하세요: "))
-    
     if balance < 0:
         print("마이너스 잔액")
     elif balance == 0:
         print("잔액 없음")
     else:
         print("정상 잔액")
-
-balance_status()
